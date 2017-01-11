b543c62162ac16e9feed0fe255e5e3318598f557
abel-valle <a.valle.chavez@gmail.com>
2017-01-11 17:38:11 -0600

    Dependencia para filtro CORS.

--- authentication-service-webapp/src/test/java/com/lux/authentication/UsuarioTest.java
+++ authentication-service-webapp/src/test/java/com/lux/authentication/UsuarioTest.java
@@ -11,7 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.ws.rs.client.*;
 import javax.ws.rs.core.MediaType;
